3aaa35abff2cf5275e5e00a47d3d581006f90777
Shota Narisaka <shootacean@gmail.com>
2022-03-10 08:58:03 +0900

    add volta

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,10 @@ if status is-interactive
     eval (/opt/homebrew/bin/brew shellenv)
 end
 
+# Path
+set VOLTA_HOME $HOME/.volta
+set PATH $VOLTA_HOME/bin $PATH
+
 # peco
 function fish_user_key_bindings
     bind \cr peco_select_history
