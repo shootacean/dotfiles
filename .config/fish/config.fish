e83906a0d0c2560c5698c3e5b5899aab41da0f29
Shota Narisaka <shootacean@gmail.com>
2022-03-10 08:55:11 +0900

    add fisher, peco, ghq

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     eval (/opt/homebrew/bin/brew shellenv)
 end
+
+# peco
+function fish_user_key_bindings
+    bind \cr peco_select_history
+end
+
+# GHQ
+set GHQ_SELECTOR peco
+
+# Append
